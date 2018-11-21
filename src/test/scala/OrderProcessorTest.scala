6cd8ded9350247f8bd3f294d25dfb95a906e6ea2
chaitanyawaikar <33855574+chaitanyawaikar@users.noreply.github.com>
2018-11-21 23:48:50 +0530

    Add files via upload

--- src/test/scala/OrderProcessorTest.scala
+++ src/test/scala/OrderProcessorTest.scala
@@ -1,6 +1,17 @@
 import org.scalatest.FunSuite
+import org.scalatest.mockito.MockitoSugar
 
-class OrderProcessorTest extends FunSuite {
+import scala.collection.mutable.ArrayBuffer
 
+class OrderProcessorTest extends FunSuite with MockitoSugar{
 
+  test(" should calculate the average waiting time given the list of customers in the order in which they arrive for input1"){
+    val customerList = ArrayBuffer(Customer(0,3), Customer(1,9), Customer(2,6))
+    assert(OrderProcessor.calculateAverageWaitingTime(customerList) == 9)
+  }
+
+  test("should calculate the average waiting time given the list of customers in the order in which they arrive for input2"){
+    val customerList = ArrayBuffer(Customer(0,3), Customer(1,9), Customer(2,5))
+    assert(OrderProcessor.calculateAverageWaitingTime(customerList) == 8)
+  }
 }
